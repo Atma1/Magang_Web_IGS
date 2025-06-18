39707b12d7f101c4c43c9ce17813d92c176106d8
Atma1 <atmasusanto08@gmail.com>
2025-06-18 21:43:32 +0800

    feat: update MapFilter component to utilize filter prop for button styling

--- src/components/map/MapFilter.tsx
+++ src/components/map/MapFilter.tsx
@@ -2,38 +2,51 @@ import React from 'react';
 
 interface MapFilterProps {
   onFilterChange: (status: string | null) => void;
+  filter: string | null
 }
 
-const MapFilter: React.FC<MapFilterProps> = ({ onFilterChange }) => {
+const MapFilter: React.FC<MapFilterProps> = ({ onFilterChange, filter }) => {
   return (
     <div className="flex flex-wrap items-center justify-between">
       <div className="text-gray-700 font-medium mb-2 sm:mb-0">Filter sensors by status:</div>
-      
+
       <div className="flex space-x-2">
         <button
           onClick={() => onFilterChange(null)}
-          className="px-3 py-1 rounded-md text-sm font-medium bg-gray-100 text-gray-600 hover:bg-gray-200"
+          className={`px-3 py-1 rounded-md text-sm font-medium ${filter === null
+            ? 'bg-gray-200 text-gray-800'
+            : ''
+            }`}
         >
           All
         </button>
-        
+
         <button
           onClick={() => onFilterChange('Normal')}
-          className="px-3 py-1 rounded-md text-sm font-medium bg-success-100 text-success-600 hover:bg-success-200"
+          className={`px-3 py-1 rounded-md text-sm font-medium ${filter === 'Normal'
+            ? 'bg-gray-200 text-gray-800'
+            : ''
+            }`}
         >
           Normal
         </button>
-        
+
         <button
           onClick={() => onFilterChange('Siaga')}
-          className="px-3 py-1 rounded-md text-sm font-medium bg-warning-100 text-warning-600 hover:bg-warning-200"
+          className={`px-3 py-1 rounded-md text-sm font-medium ${filter === 'Siaga'
+            ? 'bg-gray-200 text-gray-800'
+            : ''
+            }`}
         >
           Siaga
         </button>
-        
+
         <button
           onClick={() => onFilterChange('Bahaya')}
-          className="px-3 py-1 rounded-md text-sm font-medium bg-danger-100 text-danger-600 hover:bg-danger-200"
+          className={`px-3 py-1 rounded-md text-sm font-medium ${filter === 'Bahaya'
+            ? 'bg-gray-200 text-gray-800'
+            : ''
+            }`}
         >
           Bahaya
         </button>
