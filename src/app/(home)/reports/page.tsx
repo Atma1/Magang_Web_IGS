c069e71d28bcacd661cc6552e72ddfbc9f9f884a
Atma1 <atmasusanto08@gmail.com>
2025-06-27 18:09:43 +0800

    feat: removed unused import

--- src/app/(home)/reports/page.tsx
+++ src/app/(home)/reports/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useState } from 'react';
 import { motion } from 'framer-motion';
 import ReportForm from '../../../components/reports/ReportForm';
 import ReportsList from '../../../components/reports/ReportsList';
